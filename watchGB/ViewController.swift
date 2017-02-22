97a75d3761ceb80a6777f9ef2662fc53b7f7748b
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2017-02-22 22:17:01 +0900

    Initial Commit

---
+++ watchGB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchGB
+//
+//  Created by Gabriel O'Flaherty-Chan on 2017-02-22.
+//  Copyright © 2017 Gabrieloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
